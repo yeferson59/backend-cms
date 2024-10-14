fbd6c1f0bd41c8db7b3803c01e607d9dd36708f7
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-10-13 23:02:32 -0500

    add new schemas and add plugin of cloudinary

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,20 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Struct, Schema } from '@strapi/strapi';
+
+export interface ContentBlocks extends Struct.ComponentSchema {
+  collectionName: 'components_content_blocks';
+  info: {
+    displayName: 'Rich text';
+    description: '';
+  };
+  attributes: {
+    body: Schema.Attribute.Blocks & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'content.blocks': ContentBlocks;
+    }
+  }
+}
