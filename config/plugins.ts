eb7f96052651b32ff863ed68445080de5d2dce25
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-12-19 12:56:56 -0500

    add redis

--- config/plugins.ts
+++ config/plugins.ts
@@ -13,4 +13,55 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  redis: {
+    config: {
+      connections: {
+        default: {
+          connection: {
+            host: "redis.railway.internal",
+            port: 6379,
+            db: 0,
+          },
+          settings: {
+            debug: false,
+          },
+        },
+      },
+    },
+  },
+  // Step 2: Configure the redis cache plugin
+  "rest-cache": {
+    config: {
+      provider: {
+        name: "redis",
+        options: {
+          max: 32767,
+          connection: "default",
+        },
+      },
+      strategy: {
+        enableEtagSupport: true,
+        logs: true,
+        clearRelatedCache: true,
+        maxAge: 3600000,
+        contentTypes: [
+          // list of Content-Types UID to cache
+          "api::category.category",
+          "api::article.article",
+          "api::global.global",
+          "api::homepage.homepage",
+          {
+            contentType: "api::category.category",
+            maxAge: 3600000,
+            hitpass: false,
+            keys: {
+              useQueryParams: false,
+              useHeaders: ["accept-encoding"],
+            },
+            method: "GET",
+          },
+        ],
+      },
+    },
+  },
 });
