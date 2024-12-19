2057427f2080e3097fabe69371fe4a1c2b06b669
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2024-12-19 13:20:05 -0500

    update plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -19,8 +19,8 @@ module.exports = ({ env }) => ({
         default: {
           connection: {
             host: env("REDIS_HOST", "redis.railway.internal"),
-            port: env.int("REDIS_PORT", 6379),
-            db: env.int("REDIS_DB", 0),
+            port: env("REDIS_PORT", 6379),
+            db: env("REDIS_DB", 0),
             password: env("REDIS_PASSWORD", null), // Añade la contraseña
           },
           settings: {
